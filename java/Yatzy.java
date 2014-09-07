9a193de8ffc97712107c374f61db0a3a8bf03228
Alvaro Garcia <derecerca@gmail.com>
2014-09-07 03:25:32 +0200

    refactor: organize two_pair so it's clearer

--- java/Yatzy.java
+++ java/Yatzy.java
@@ -84,10 +84,10 @@ public class Yatzy {
         int[] counts = getCounts(d1, d2, d3, d4, d5);
         int n = 0;
         int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6-i-1] >= 2) {
+        for (int i = 5; i >= 0; i--)
+            if (counts[i] >= 2) {
                 n++;
-                score += (6-i);
+                score += (i+1);
             }        
         if (n == 2)
             return score * 2;
