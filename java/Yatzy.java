cf4e7db527b37946bf14b54d28fac94bc19e2871
Alvaro Garcia <derecerca@gmail.com>
2014-09-07 02:24:38 +0200

    refactor: threes to method that can be optimized to using a loop

--- java/Yatzy.java
+++ java/Yatzy.java
@@ -59,15 +59,20 @@ public class Yatzy {
     }
 
     public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;    
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
-        return s;
+        return addThrees(new Integer[]{d1, d2, d3, d4, d5});
     }
+
+	private static int addThrees(Integer[] integers) {
+		int s;    
+        s = 0;
+        if (integers[0] == 3) s += 3;
+        if (integers[1] == 3) s += 3;
+        if (integers[2] == 3) s += 3;
+        if (integers[3] == 3) s += 3;
+        if (integers[4] == 3) s += 3;
+        return s;
+	}
+
 
 	public int fours()
     {
