8d438f4cbf52afe295a9d5b7364d7d88cb20227d
Alvaro Garcia <derecerca@gmail.com>
2014-09-07 02:15:42 +0200

    refactor: move sut to test class attribute

--- java/YatzyTest.java
+++ java/YatzyTest.java
@@ -3,10 +3,17 @@ import static org.junit.Assert.*;
 
 public class YatzyTest {
 
-    @Test
+    private Yatzy sut;
+    
+    @Before
+    public void setUp(){
+    	sut = new Yatzy();
+    }
+
+	@Test
     public void chance_scores_sum_of_all_dice() {
         int expected = 15;
-        int actual = new Yatzy().chance(2,3,4,5,1);
+		int actual = sut.chance(2,3,4,5,1);
         assertEquals(expected, actual);
         assertEquals(16, Yatzy.chance(3,3,4,5,1));
     }
