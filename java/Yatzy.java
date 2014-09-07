7d6585c7390555cd14ad1ee1b03d85f69659f30a
Alvaro Garcia <derecerca@gmail.com>
2014-09-07 02:38:01 +0200

    refactor: inline intermediate function addTwos

--- java/Yatzy.java
+++ java/Yatzy.java
@@ -50,12 +50,8 @@ public class Yatzy {
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        return addTwos(new Integer[]{d1, d2, d3, d4, d5});
+        return sumMatchingValues(new Integer[]{d1, d2, d3, d4, d5}, 2);
     }
-
-	private static int addTwos(Integer[] integers) {
-		return sumMatchingValues(integers, 2);
-	}
 
 	private static int sumMatchingValues(Integer[] integers, int value) {
 		int s = 0;
